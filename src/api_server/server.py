80ad6d7671e311c5b3dc91a10e945928d25d093c
Maxim <maxkekino@mail.ru>
2026-02-08 11:28:06 +0300

    тест для rebase 2

--- src/api_server/server.py
+++ src/api_server/server.py
@@ -176,7 +176,7 @@ class UserList(Resource):
         users.append(new_user)
         return serialize_data(new_user), 201
 
-# просто так 2
+# просто так 3
 @ns.route('/users/<int:user_id>')
 @ns.response(404, 'User not found')
 @ns.param('user_id', 'The user identifier')
